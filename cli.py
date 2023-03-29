16d31700118d0d01fd4a595fc624ad33244accd3
K4r1it0 <39193373+K4r1it0@users.noreply.github.com>
2023-03-29 04:55:08 +0200

    Update cli.py
    v1

--- cli.py
+++ cli.py
@@ -2,7 +2,12 @@ import json
 import requests
 import sys
 
-data = requests.get(f"http://139.144.225.136:5000/ssl?search_string={sys.argv[1]}").json()
+
+if len(sys.argv) < 2:
+    print("Error: Please provide a keyword : python cli.py 'keyword' ")
+    sys.exit()
+
+data = requests.get(f"http://139.144.225.136:5050/ssl?search_string={sys.argv[1]}").json()
 
 domains = set()  # Create a set to store the extracted domains
 
@@ -10,22 +15,23 @@ for item in data:
     issuer_domain = ''
     subject_domain = ''
     subject_alt_domains = []
+    host = item.get('host', '')
     try:
-        issuer_domain = item.get('issuer', '').split('CN=')[1]
-    except (IndexError, KeyError) as e:
-        print(f"Error while extracting issuer domain: {e}")
+        issuer_domain = item.get('issuer', '').split('CN=')[1].split(';')[0].strip().replace("*.","")
+    except (IndexError, KeyError):
+        pass
     try:
-        subject_domain = item.get('subject', '').split('CN=')[1]
-    except (IndexError, KeyError) as e:
-        print(f"Error while extracting subject domain: {e}")
+        subject_domain = item.get('subject', '').split('CN=')[1].split(';')[0].strip().replace("*.","")
+    except (IndexError, KeyError):
+        pass
     try:
         subject_alt_names = item.get('subjectAltName', '').split(',')
         for name in subject_alt_names:
             if 'DNS:' in name:
-                subject_alt_domains.append(name.split('DNS:')[1])
-    except (IndexError, KeyError) as e:
-        print(f"Error while extracting subject alternative names: {e}")
-    domains.update([issuer_domain, subject_domain] + subject_alt_domains)
+                subject_alt_domains.append(name.split('DNS:')[1].strip().replace("*.",""))
+    except (IndexError, KeyError):
+        pass
+    domains.update([domain for domain in [issuer_domain, subject_domain, host] + subject_alt_domains if domain])
 
 # Print the unique domains
 for domain in domains:
